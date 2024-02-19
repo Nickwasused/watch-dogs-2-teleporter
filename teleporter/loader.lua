760e7a4f14a8cb20aef6b1dffdf3fe6a9e430a7e
Nickwasused <github.lix1s@simplelogin.co>
2024-02-19 15:36:14 +0100

    fix unloading of mission layers

--- teleporter/loader.lua
+++ teleporter/loader.lua
@@ -3,7 +3,9 @@ local script = Script()
 local tp_state = {
 	target_cord_x = nil,
 	target_cord_y = nil,
-	target_cord_z = nil
+	target_cord_z = nil,
+	current_mission = nil,
+	last_mission = nil
 }
 script.tp_state = tp_state
 tp_state._type = tp_state
@@ -35,6 +37,7 @@ function tp_state:LoadLayer_and_tp(name, x, y, z, mission)
 	script.tp_state.target_cord_z = z
 
 	if mission then
+		script.tp_state.current_mission = name
 		LoadMissionLayer2("0", tp_state, "on_loaded", name)
 	else
 		LoadLMALayer(name, "0", 1, tp_state, "on_loaded")
@@ -50,9 +53,34 @@ function tp_state:on_loaded()
 	elseif script.tp_state.target_cord_z == nil then
 		ScriptHook.ShowNotification("There was an error!", 15)
 	else
+		-- teleport
 		SetEntityPosition(GetLocalPlayerEntityId(), script.tp_state.target_cord_x, script.tp_state.target_cord_y, script.tp_state.target_cord_z)
+		-- unload the last loaded mission
+		script.tp_state:unload_last_mission()
+		-- set the last mission to the current mission
+		script.tp_state.last_mission = script.tp_state.current_mission
 	end
 end
+
+-- unload the last loaded mission
+function tp_state:unload_last_mission()
+	local last_mission = script.tp_state.last_mission
+	local current_mission = script.tp_state.current_mission
+	if last_mission == current_mission then
+		print("nothing to unload")
+	else
+		if last_mission == nil then
+			print("there is no last mission")
+		else
+			print("unloading " .. last_mission)
+			ScriptHook.ShowNotification("unloading " .. last_mission)
+			UnloadMissionLayer2("0", tp_state, "", last_mission)
+			script.tp_state.last_mission = current_mission
+			script.tp_state.current_mission = nil
+		end
+	end
+end
+
 
 function just_tp(x, y, z)
 	SetEntityPosition(GetLocalPlayerEntityId(), x, y, z)
@@ -70,5 +98,7 @@ end
 
 -- unload the last loaded mission layer on script unload
 function script:OnUnload()
-
+	-- set this to bypass unload check
+	script.tp_state.current_mission = "nickwasused"
+	script.tp_state:unload_last_mission()
 end
